8009207b0bdad2ed737334ceb10963024a158cf3
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-08-20 09:22:57 +0100

    align contacts left

--- client/src/pages/contact/contact.styles.jsx
+++ client/src/pages/contact/contact.styles.jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const ContactPageContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
 `;
 
 export const TitleContainer = styled.span`
